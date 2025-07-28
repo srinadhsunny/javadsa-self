dfe286e0a84eb110588c78d83826b9a923a9097e
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-07-28 15:39:08 +0530

    Add files via upload

---
+++ patterns/half_triangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class half_triangle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter row num");
+        int r = sc.nextInt();
+        for(int i=1;i<=r;i++){
+             System.out.println();
+            
+            for(int j=1;j<=i;j++){
+                    System.out.print("*");
+                
+        }
+    }
+}
+}
