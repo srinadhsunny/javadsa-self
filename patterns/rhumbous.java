1cf64bb8760dd85dde612880f61747797d85b73a
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-07-29 17:03:47 +0530

    Update rhumbous.java

--- patterns/rhumbous.java
+++ patterns/rhumbous.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class rhumbous {
     public static void main(String[] args) {
-        Scanner sc = new Scanner;
+        Scanner sc = new Scanner(System.in);
         int n =sc.nextInt;
         for(int i=1;i<=n;i++){
             int spaces=n-i;
