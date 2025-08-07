76dc980629acb1538af7eea3dc297c37e63c169c
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-08-07 20:26:40 +0530

    Add files via upload

---
+++ string/compareStrings.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class compareStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //input fyst string
+        System.out.println("enter the fyst string");
+        String s1= sc.nextLine();
+        //input secound string
+        System.out.println("enter the secound string");
+        String s2=sc.nextLine();
+        //compare the String s1.compareTo(s2)returns
+        //->+ve if s1>s2
+        //->0 if s11=s2
+        //-> -ve if s1<s2
+        if(s1.compareTo(s2)==0){
+            System.out.println("Strings are equal");
+        }else if(s1.compareTo(s2)<0){
+            System.out.println(s1 + " length is greater ");
+        }else{
+            System.out.println(s2 +"length is greater");
+        }
+
+        
+    }
+    
+
+    
+}
