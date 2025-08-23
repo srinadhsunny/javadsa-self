99038043b7db85a8c0a998d149c20e07dbe05938
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-08-23 21:34:05 +0530

    Add files via upload

---
+++ oops /constructors/constructor.java
@@ -0,0 +1,23 @@
+class student{
+    String name;
+    int age;
+    public void printinfo(){
+        System.out.println(name);
+        System.out.println(age);
+        //constructor is called when  new is used to create instances(particular object storage) //same as class withput return type
+    }
+        student(){
+            System.out.println("constructor called");
+        }
+    
+}
+public class constructor {
+
+    public static void main(String[] args) {
+        student s1 = new student();
+        //constructor is used above that we created
+        s1.name="sunny";
+        s1.age=20;
+        s1.printinfo();
+    }
+}
