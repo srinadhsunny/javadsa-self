da7e749e65818387fc3f5e0a74e5fddb36bea4c4
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-08-24 20:05:49 +0530

    Update interfaces.java

--- oops/interfaces/interfaces.java
+++ oops/interfaces/interfaces.java
@@ -1,3 +1,4 @@
+//interface is a pyre abstraction which makes multiple interface class to be implemented in a class
 interface Animal{
     void walk();
     int eyes=2;//final its can,t be changed it fix's that  evry animal has 2 eyes
@@ -5,7 +6,9 @@ interface Animal{
 interface herbivours{
 
 }
+//class that implements to interface classes
 class Horse implements Animal,herbivours{
+    //methods are implemented by the derived class but we need to define here
     public void walk(){
         System.out.println("horse walks with 4 legs");
     }
@@ -13,8 +16,9 @@ class Horse implements Animal,herbivours{
 public class interfaces{
     public static void main(String[] args) {
         Horse h1 = new Horse();
-        h1.walk();
-       System.out.println(h1.eyes);
+        h1.walk();//sinces it is a method
+       System.out.println(h1.eyes);//sinces eyes is an intialization
     }
     
 }
+
