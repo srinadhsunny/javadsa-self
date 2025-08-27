83767ce054bbdc65fcaccd3eb52037b887eb58bc
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-08-27 21:06:36 +0530

    Update container-with-most-water-own.java

--- leetcode-daywise-problems/container-with-most-water-own.java
+++ leetcode-daywise-problems/container-with-most-water-own.java
@@ -13,3 +13,27 @@ class Solution {
     }return max;
 }
 }
+//optimized to improve time complexity
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int i=0;
+        int res=0;
+        int j=n-1;
+        int area;
+        while(i<j){
+            area= Math.min(height[i],height[j])*(j-i);
+        if(res<area){
+            res=area;
+        }
+        
+        if(height[i]>height[j]){
+            j--;
+        }else{
+            i++;
+        }
+        }
+        
+    return res;
+    }
+}
