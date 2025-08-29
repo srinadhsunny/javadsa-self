4ccc374134db46b1b779858125f2f8557e9d0977
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-08-29 21:34:32 +0530

    Create 3sum.java

---
+++ leetcode-daywise-problems/3sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> ans= new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length&&j!=i;j++){
+                for(int k=j+1;k<nums.length&&k!=i&&k!=j;k++){
+                    if(nums[i]==-(nums[j]+nums[k])){
+                         List<Integer> res= new ArrayList<>();
+                         res.add(nums[i]);
+                         res.add(nums[j]);
+                         res.add(nums[k]);
+                         Collections.sort(res);
+                         ans.add(res);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+}
