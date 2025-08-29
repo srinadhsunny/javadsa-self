5ee18fc887499316d2f6b60ef95ae295ed19e89c
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-08-29 21:47:36 +0530

    Update 3sum.java

--- leetcode-daywise-problems/3sum.java
+++ leetcode-daywise-problems/3sum.java
@@ -19,3 +19,40 @@ class Solution {
         return new ArrayList<>(ans);
     }
 }
+// code is optimized to reduce compleccity and saving time
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums); // Step 1: sort array
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++; // need bigger sum
+                } else {
+                    right--; // need smaller sum
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
