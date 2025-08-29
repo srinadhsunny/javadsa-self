1e2b794de8a63cb4e1c8e07ed1d9c083e4afb3d5
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-08-29 21:35:13 +0530

    Update 3sum.java

--- leetcode-daywise-problems/3sum.java
+++ leetcode-daywise-problems/3sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        //set can be used such that it automaticaly removes duplicates
         Set<List<Integer>> ans= new HashSet<>();
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length&&j!=i;j++){
